75234179f56c8fe45609c9179db7548ee9e1507b
Your Name <you@example.comclear>
2025-04-12 13:46:10 +0530

    Added url of deployed front end into the server.js file

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ const weatherRoute = require('./routes/weather');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://real-time-weather-dashboard-dun.vercel.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/weather', weatherRoute);
 
